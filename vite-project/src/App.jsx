af978175c7523d232e932ff352ca08c0a6efa11c
NzamaE <nzamaernest1@gmail.com>
2025-09-22 09:34:09 +0200

    Added learderboard page

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,16 +7,20 @@ import Timeline06 from '@/components/Timeline-06/timeline-06'
 import Footer05 from '@/components/Footer-05/footer-05'
 import Login02 from '@/components/login-02/login-02'
 import SignUp02 from '@/components/signup-02/signup-02'
+
 import Dashboard from '@/components/dashboard/dashboard'
+import Leaderboard from '@/components/leaderboard/leaderboard' // Add this import
+
 import './App.css'
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/signup" element={<SignUp02 />} />
+        <Route path="/signup" element={<SignUp02 />} />
         <Route path="/login" element={<Login02 />} />
-         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/leaderboard" element={<Leaderboard />} /> {/* Add this route */}
         <Route path="/" element={
           <>
             <Navbar />
